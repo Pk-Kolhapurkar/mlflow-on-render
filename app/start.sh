13d982f4aaede9f87f9735119c8d92e1be28f57d
Prathamesh Khade <121573184+Pk-Kolhapurkar@users.noreply.github.com>
2024-12-27 23:45:43 +0530

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export BACKEND_STORE_URI=<Your PostgreSQL URL>   # Replace with your Render database URL
-export ARTIFACT_STORE_URI=<Your S3/GCS URI>      # Replace with your S3/GCS bucket URI
+export BACKEND_STORE_URI=<postgresql://pk:fEqmHb5cCTtvRpeOCGERx9amFXn9Ilv8@dpg-ctnel85umphs73c5ufm0-a.singapore-postgres.render.com/mlflow_0fna>   # Replace with your Render database URL
+#export ARTIFACT_STORE_URI=<Your S3/GCS URI>      # Replace with your S3/GCS bucket URI
 
 # Start MLflow server
 mlflow server \
     --backend-store-uri $BACKEND_STORE_URI \
-    --default-artifact-root $ARTIFACT_STORE_URI \
+   
     --host 0.0.0.0 \
     --port 5000
 
